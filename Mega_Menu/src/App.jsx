1ffa0324f8ed9f3b863be54839b356781c0f4619
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-11 10:45:12 +0430

    Setup the project with Reacct + Tailwind Css

---
+++ Mega_Menu/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto nulla voluptatum voluptas architecto fugit fuga tempore assumenda totam repellat sequi reprehenderit repudiandae, molestiae nostrum mollitia impedit quod. Hic, reprehenderit necessitatibus.</p>
+   </div>
+  )
+}
+
+export default App
